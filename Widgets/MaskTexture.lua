c2d5caa8dbe9e8d812e751488022a5d6ddaf4bf3
shludengzhun@corp.netease.com <shludengzhun@corp.netease.com>
2022-01-11 13:29:54 +0800

    update

---
+++ Widgets/MaskTexture.lua
@@ -0,0 +1,3 @@
+---@meta
+---@class MaskTexture: Texture
+local MaskTexture = {}
---
+++ classic/Enum.lua
@@ -0,0 +1,6 @@
+---@meta
+---@class ItemQuality
+local ItemQuality = {}
+
+ItemQuality.Good = Enum.ItemQuality.Uncommon
+ItemQuality.Superior = Enum.ItemQuality.Rare
